b7d684ed1c822fd40587525f54792ee71aef792e
Romel Pérez <ronelprhone@gmail.com>
2016-07-23 21:16:59 -0500

    feat: views and 404 page

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const app = express();
 const port = process.env.PORT || 80;
 
-app.use(express.static(`${__dirname}/assets`));
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+app.use(express.static('./assets'));
+
+app.use((req, res, next) => {
+  res.render('404.html');
+});
 
 app.listen(port, function (err) {
   if (err) throw err;
