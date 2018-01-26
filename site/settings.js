8cf176c42ea65ed609b5d3ddc61c4c4631fc5282
Romel Pérez <ronelprhone@gmail.com>
2018-01-25 21:46:01 -0500

    [feat] website

---
+++ site/settings.js
@@ -0,0 +1,10 @@
+const baseTitle = 'Romel Pérez - Personal Website';
+module.exports.titles = Object.assign(
+  {
+    '/': baseTitle,
+  }
+);
+
+module.exports.googleAnalytics = 'UA-50433259-1';
+
+module.exports.default = {};
---
+++ site/withTemplate.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { lighten, darken } from 'polished';
+import {
+  ThemeProvider,
+  createTheme,
+  SoundsProvider,
+  createSounds
+} from 'arwes';
+
+import Template from './components/Template';
+
+const resources = {
+  background: {
+    small: '/static/img/background-small.jpg',
+    medium: '/static/img/background-medium.jpg',
+    large: '/static/img/background-large.jpg',
+    xlarge: '/static/img/background-xlarge.jpg'
+  },
+  pattern: '/static/img/glow.png',
+};
+
+const generateColor = color => ({
+  base: color,
+  light: lighten(0.2, color),
+  dark: darken(0.2, color),
+});
+const generateBackground = color => ({
+  level0: color,
+  level1: lighten(0.015, color),
+  level2: lighten(0.030, color),
+  level3: lighten(0.045, color),
+});
+const theme = {
+  color: {
+    primary: generateColor('#30fffe'),
+  },
+  background: {
+    primary: generateBackground('#031212'),
+  },
+};
+
+const sounds = {
+  shared: {
+    volume: 0.6,
+  },
+  players: {
+    click: {
+      sound: { src: ['/static/sound/click.mp3'] },
+      settings: { oneAtATime: true }
+    },
+    typing: {
+      sound: { src: ['/static/sound/typing.mp3'] },
+      settings: { oneAtATime: true }
+    },
+    deploy: {
+      sound: { src: ['/static/sound/deploy.mp3'] },
+      settings: { oneAtATime: true }
+    },
+  }
+};
+
+export default (App) => {
+  return (props) => (
+    <ThemeProvider theme={createTheme(theme)}>
+      <SoundsProvider sounds={createSounds(sounds)}>
+        <Template>
+          <App resources={resources} {...props} />
+        </Template>
+      </SoundsProvider>
+    </ThemeProvider>
+  );
+};
