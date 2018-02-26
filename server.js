d7153ffaa538c16b073c5fdcc11c9642621427c2
Romel Pérez <ronelprhone@gmail.com>
2018-02-26 02:41:47 -0500

    [chore] update deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = require('next-routes')();
 
 const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 7000;
 const app = next({ dev });
 const handler = routes.getRequestHandler(app);
 
@@ -11,5 +12,5 @@ routes.add('/talks/:talkKey', 'talks');
 app.prepare().then(() => {
   const server = express();
   server.use(handler);
-  server.listen(7000);
+  server.listen(port);
 });
