f423fd3dc5064bc71618bcfad985fe8eb0352fa2
Romel Pérez <ronelprhone@gmail.com>
2018-06-06 00:58:14 -0500

    [feat] add carrera-open-source talk

--- site/settings.js
+++ site/settings.js
@@ -1,6 +1,7 @@
 const projects = require('./data/projects');
 const talk1 = require('./data/testing-javascript');
 const talk2 = require('./data/css-in-js-with-jss-and-react');
+const talk3 = require('./data/como-iniciar-tu-carrera-en-programacion-con-open-source');
 
 const baseTitle = 'Romel Pérez - Personal Website';
 module.exports.titles = {
@@ -12,6 +13,6 @@ module.exports.googleAnalytics = 'UA-50433259-1';
 
 module.exports.projects = projects;
 
-module.exports.talks = [talk1, talk2];
+module.exports.talks = [talk1, talk2, talk3];
 
 module.exports.default = {};
