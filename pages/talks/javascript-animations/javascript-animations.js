6412f533b23611a57ee98eb5275b175ac7037bb6
Romel Pérez <ronelprhone@gmail.com>
2018-10-09 17:00:43 -0500

    [update] talk JavaScript Animations

--- pages/talks/javascript-animations/javascript-animations.js
+++ pages/talks/javascript-animations/javascript-animations.js
@@ -79,6 +79,39 @@ export default {
     {
       children: [{
         element: 'ImagePlain',
+        props: { src: '/static/talks/javascript-animations/canvas.png' }
+      }, {
+        element: 'Heading',
+        props: { 'node': 'h3', 'style': { fontSize: '62px' } },
+        children: 'Canvas and WebGL'
+      }, {
+        element: 'Paragraph',
+        children: 'Draw bitmap graphs on 2D and 3D'
+      }]
+    },
+    {
+      children: [{
+        element: 'List',
+        children: [{
+          element: 'li',
+          children: [{
+            element: 'Link',
+            props: { href: 'https://codepen.io/joniibra/pen/dGzMPq', target: '_blank' },
+            children: 'BACKGROUND: Animated Flow Neon'
+          }]
+        }, {
+          element: 'li',
+          children: [{
+            element: 'Link',
+            props: { href: 'https://codepen.io/FrankFitzGerald/pen/LAbfm', target: '_blank' },
+            children: 'Sakura Animation using WebGL'
+          }]
+        }]
+      }]
+    },
+    {
+      children: [{
+        element: 'ImagePlain',
         props: { src: '/static/talks/javascript-animations/javascript.png' }
       }, {
         element: 'Heading',
@@ -119,38 +152,8 @@ export default {
     {
       children: [{
         element: 'ImagePlain',
-        props: { src: '/static/talks/javascript-animations/canvas.png' }
+        props: { src: '/static/talks/javascript-animations/tools.png' }
       }, {
-        element: 'Heading',
-        props: { 'node': 'h3', 'style': { fontSize: '62px' } },
-        children: 'Canvas and WebGL'
-      }, {
-        element: 'Paragraph',
-        children: 'Draw bitmap graphs on 2D and 3D'
-      }]
-    },
-    {
-      children: [{
-        element: 'List',
-        children: [{
-          element: 'li',
-          children: [{
-            element: 'Link',
-            props: { href: 'https://codepen.io/joniibra/pen/dGzMPq', target: '_blank' },
-            children: 'BACKGROUND: Animated Flow Neon'
-          }]
-        }, {
-          element: 'li',
-          children: [{
-            element: 'Link',
-            props: { href: 'https://codepen.io/FrankFitzGerald/pen/LAbfm', target: '_blank' },
-            children: 'Sakura Animation using WebGL'
-          }]
-        }]
-      }]
-    },
-    {
-      children: [{
         element: 'Heading',
         props: { 'node': 'h2', 'style': { fontSize: '74px' } },
         children: 'Libraries'
@@ -197,6 +200,9 @@ export default {
     },
     {
       children: [{
+        element: 'ImagePlain',
+        props: { src: '/static/talks/javascript-animations/react.png' }
+      }, {
         element: 'Heading',
         props: { 'node': 'h2', 'style': { fontSize: '74px' } },
         children: 'React Animations'
@@ -207,6 +213,36 @@ export default {
         element: 'Heading',
         props: { 'node': 'h3', 'style': { fontSize: '62px' } },
         children: 'Components Integration'
+      }, {
+        element: 'Paragraph',
+        children: 'Add interactivity to React components DOM elements using animation tools'
+      }]
+    },
+    {
+      children: [{
+        element: 'List',
+        children: [{
+          element: 'li',
+          children: [{
+            element: 'Link',
+            props: { href: 'https://greensock.com/react', target: '_blank' },
+            children: 'React and GreenSock'
+          }]
+        }, {
+          element: 'li',
+          children: [{
+            element: 'Link',
+            props: { href: 'https://codepen.io/romelperez/pen/bmgxZR', target: '_blank' },
+            children: 'React and GreenSock - Simple'
+          }]
+        }, {
+          element: 'li',
+          children: [{
+            element: 'Link',
+            props: { href: 'https://codepen.io/romelperez/pen/mzRQWj', target: '_blank' },
+            children: 'React and GreenSock - Multiple Elements'
+          }]
+        }]
       }]
     },
     {
@@ -214,13 +250,43 @@ export default {
         element: 'Heading',
         props: { 'node': 'h3', 'style': { fontSize: '62px' } },
         children: 'React Spring'
+      }, {
+        element: 'Paragraph',
+        children: 'A set of spring-physics based primitives'
+      }]
+    },
+    {
+      children: [{
+        element: 'List',
+        children: [{
+          element: 'li',
+          children: [{
+            element: 'Link',
+            props: { href: 'http://react-spring.surge.sh', target: '_blank' },
+            children: 'React Spring Docs'
+          }]
+        }, {
+          element: 'li',
+          children: [{
+            element: 'Link',
+            props: { href: 'https://codesandbox.io/s/kxl04n34wr', target: '_blank' },
+            children: 'Basic Spring Animation'
+          }]
+        }, {
+          element: 'li',
+          children: [{
+            element: 'Link',
+            props: { href: 'https://codesandbox.io/s/3vwoq51nl5', target: '_blank' },
+            children: 'Basic Transition Animation'
+          }]
+        }]
       }]
     },
     {
       children: [{
         element: 'Heading',
         props: { 'node': 'h2', 'style': { fontSize: '74px' } },
-        children: 'Comparing'
+        children: 'How to compare'
       }]
     },
     {
@@ -258,6 +324,44 @@ export default {
         element: 'Heading',
         props: { 'node': 'h1' },
         children: '"Evaluate what you need for your project and adjust for your team to choose an animation tool"'
+      }]
+    },
+    {
+      children: [{
+        element: 'Heading',
+        props: { 'node': 'h3', 'style': { fontSize: '62px' } },
+        children: 'Honorable mentions'
+      }, {
+        element: 'List',
+        children: [{
+          element: 'li',
+          children: [{
+            element: 'Link',
+            props: { href: 'https://developer.mozilla.org/en-US/docs/Web/API/Web_Animations_API', target: '_blank' },
+            children: 'Web Animations API'
+          }]
+        }, {
+          element: 'li',
+          children: [{
+            element: 'Link',
+            props: { href: 'https://reactcommunity.org/react-transition-group', target: '_blank' },
+            children: 'React Transition Group'
+          }]
+        }, {
+          element: 'li',
+          children: [{
+            element: 'Link',
+            props: { href: 'https://github.com/chenglou/react-motion', target: '_blank' },
+            children: 'React Motion'
+          }]
+        }, {
+          element: 'li',
+          children: [{
+            element: 'Link',
+            props: { href: 'http://velocityjs.org', target: '_blank' },
+            children: 'VelocityJS'
+          }]
+        }]
       }]
     },
     {
